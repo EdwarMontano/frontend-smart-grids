ed1cae69f1645be643afc03cee377514f7823ae0
Gerson Vizquel <aituring.app@gmail.com>
2023-11-20 17:37:41 -0500

    [fix]chart rendering issue

--- pages/analysis.vue
+++ pages/analysis.vue
@@ -5,7 +5,7 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
             <p>{{ cseries }}</p>
             <p>{{ coptions }}</p>
-            <apexchart  height="400" width="100%" type="line" :options="chartOptions" :series="series">
+            <apexchart :key="series" height="400" width="100%" type="line" :options="chartOptions" :series="series">
             </apexchart>
             <GraphTest />
             <GraphTest />
